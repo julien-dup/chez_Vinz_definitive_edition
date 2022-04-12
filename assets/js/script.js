f43e9a57fe79597cfa444c0b548c46b02ab23bb0
unknown <LHP6.jduparc@gmail.com>
2022-04-12 23:43:42 +0200

    mover scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,9 @@
-console.log("hello world")
-
 
 /* Function Modal */
 
 let card = document.querySelectorAll(".card2")
 let modal = document.querySelector(".modal")
 
-console.log(card)
-console.log(modal)
 
 card.forEach(element => {
    element.addEventListener("click", function(){
@@ -20,18 +16,58 @@ card.forEach(element => {
 });
 
 
-/* Function Toggle Menu */
+/* Function Toggle Menu Burger */
 
 function toggleMenu() {
     const navbar = document.querySelector(".navbar")
     const burger = document.getElementById("containMenuBurger")
     burger.addEventListener("click", () => {
         navbar.classList.toggle("show_nav")
-        console.log("affichage")
         burger.classList.toggle("show_nav")
     })
 }
 
 toggleMenu()
-let positionY = window.scrollY
-console.log(positionY)
+
+
+/* MOVER SCROLL */
+
+const first = document.querySelector(".navbar_link_first")
+const second = document.querySelector(".navbar_link_second")
+const third = document.querySelector(".navbar_link_third")
+const fourth = document.querySelector(".navbar_link_fourth")
+const fifth = document.querySelector(".navbar_link_fifth")
+const navbar = document.querySelector(".navbar")
+const burger = document.getElementById("containMenuBurger")
+
+
+first.addEventListener("click", function(){
+    navbar.classList.toggle("show_nav")
+    burger.classList.toggle("show_nav")
+    document.getElementById("slogan").scrollIntoView({behavior: 'smooth',  block: "end"})
+})
+
+second.addEventListener("click", function(){
+    navbar.classList.toggle("show_nav")
+    burger.classList.toggle("show_nav")
+    document.getElementById("containMenuCard").scrollIntoView({behavior: 'smooth',block: "start"})
+})
+
+third.addEventListener("click", function(){
+    navbar.classList.toggle("show_nav")
+    burger.classList.toggle("show_nav")
+    document.getElementById("Nous_trouver").scrollIntoView({behavior: 'smooth', block: "start"})
+})
+
+
+fourth.addEventListener("click", function(){
+    navbar.classList.toggle("show_nav")
+    burger.classList.toggle("show_nav")
+    document.getElementById("Nous_contacter").scrollIntoView({behavior: 'smooth', block: "start"})
+})
+
+fifth.addEventListener("click", function(){
+    navbar.classList.toggle("show_nav")
+    burger.classList.toggle("show_nav")
+    document.getElementById("Notre_histoire").scrollIntoView({behavior: 'smooth' , block: "start"})
+})
